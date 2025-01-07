3815ef14e1766d7d17437da73f22a80631fb5de8
Louisa-Snyman <235snyman@gmail.com>
2025-01-07 18:12:45 +0200

    added new asteroids after shooting to main.py

--- asteroids.py
+++ asteroids.py
@@ -1,5 +1,6 @@
-import pygame
+import pygame, random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroids(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +11,16 @@ class Asteroids(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        vector_1 = pygame.math.Vector2.rotate(self.velocity, random_angle)
+        vector_2 = pygame.math.Vector2.rotate(self.velocity, random_angle * -1)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroids(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroids(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity = vector_1 * 1.2
+        asteroid_2.velocity = vector_2 * 1.2
